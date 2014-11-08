ac53d477bd92da503361908b6d595127393c944e
a-know <a.know.3373@gmail.com>
2014-11-08 13:46:41 +0900

    再生データをマップ化

--- main.go
+++ main.go
@@ -21,6 +21,9 @@ func main() {
 
 	reader := bufio.NewReaderSize(fp, 4096)
 
+	var m map[string]string = map[string]string{}
+	var shouldSet bool
+	var key string
 	d := xml.NewDecoder(reader)
 	for {
 		token, err := d.Token()
@@ -33,38 +36,90 @@ func main() {
 		}
 		switch token.(type) {
 		case xml.StartElement:
-			//do something
-			fmt.Println("StartElement")
-			fmt.Println(token.(xml.StartElement))
+			if token.(xml.StartElement).Name.Local == "key" {
+				shouldSet = false
+			} else {
+				shouldSet = true
+			}
 		case xml.EndElement:
-			//do something
-			fmt.Println("EndElement")
-			fmt.Println(token.(xml.EndElement))
+			//do nothing
 		case xml.CharData:
-			//do something
-			fmt.Println("CharData")
-			fmt.Println(string(token.(xml.CharData)))
+			if string(token.(xml.CharData)) == "Track ID" {
+				fmt.Println(m)
+				m = map[string]string{}
+			}
+			if shouldSet && key != "" {
+				m[key] = string(token.(xml.CharData))
+				key = ""
+			} else {
+				key = string(token.(xml.CharData))
+			}
 		case xml.Comment:
-			//do something
-			fmt.Println("Comment")
-			fmt.Println(string(token.(xml.Comment)))
+			//do nothing
 		case xml.ProcInst:
-			//do something
-			fmt.Println("ProcInst")
-			fmt.Println(token.(xml.ProcInst))
+			//do nothing
 		case xml.Directive:
-			//do something
-			fmt.Println("Directive")
-			fmt.Println(string(token.(xml.Directive)))
+			//do nothing
 		default:
 			panic("unknown xml token.")
 		}
 	}
 }
 
-// type Dict struct {
-// 	Thumb Thumb `xml:"thumb"`
-// }
+type playData struct {
+	Artist               string
+	Album                string
+	Genre                string
+	Kind                 string
+	Size                 string
+	TotalTime            string
+	TrackNumber          string
+	DateModified         string
+	DateAdded            string
+	BitRate              string
+	SampleRate           string
+	PersistentID         string
+	TrackType            string
+	Location             string
+	FileFolderCount      string
+	LibraryFolderCount   string
+	PlayCount            string
+	PlayDate             string
+	PlayDateUTC          string
+	Rating               string
+	AlbumRating          string
+	AlbumRatingComputed  string
+	ArtworkCount         string
+	SkipCount            string
+	SkipDate             string
+	Disabled             string
+	SortAlbum            string
+	Year                 string
+	Comments             string
+	SortName             string
+	SortArtist           string
+	VolumeAdjustment     string
+	AlbumArtist          string
+	DiscNumber           string
+	DiscCount            string
+	TrackCount           string
+	ReleaseDate          string
+	Protected            string
+	Purchased            string
+	Compilation          string
+	Composer             string
+	HasVideo             string
+	VideoWidth           string
+	VideoHeight          string
+	Movie                string
+	Master               string
+	PlaylistID           string
+	PlaylistPersistentID string
+	Visible              string
+	AllItems             string
+	PlaylistItems        string
+	SmartCriteria        string
+}
 
 // type Thumb struct {
 // 	Title  string `xml:"title"`
