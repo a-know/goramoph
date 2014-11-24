2ed96698322e56590c950c104a211dd6d3540ae2
a-know <a.know.3373@gmail.com>
2014-11-24 12:11:13 +0900

    bqへのロードを行う

--- main.go
+++ main.go
@@ -127,7 +127,7 @@ func main() {
 	fmt.Printf("in all caps: %q\n", ds_out.String())
 	fmt.Printf("ds_name: %q\n", ds_name)
 
-	if m2, _ := regexp.MatchString("\n  "+ds_name+"  \n", ds_out.String()); m2 {
+	if m2, _ := regexp.MatchString("\n  "+ds_name+"\\s+\n", ds_out.String()); m2 {
 		fmt.Println("データセット作成済み")
 	} else {
 		fmt.Println("データセット未作成")
@@ -135,7 +135,26 @@ func main() {
 		failOnError(cmd.Run())
 		fmt.Println("データセット作成完了")
 	}
+	//データセット内に既にテーブルがあるかどうかを調べ、なかったときだけロードを実施する（追記されてしまうため）
+	cmd = exec.Command("bq", "ls", ds_name)
+	var ds_ls_out bytes.Buffer
+	cmd.Stdout = &ds_ls_out
+	failOnError(cmd.Run())
+	fmt.Printf("in all caps: %q\n", ds_ls_out.String())
 
+	if m3, _ := regexp.MatchString("\n\\s+"+mod_date+"\\s+TABLE\\s+\n", ds_ls_out.String()); m3 {
+		fmt.Println("当該データロード済み")
+	} else {
+		fmt.Println("当該データ未ロード")
+		//作成したデータセットにデータをロードする
+		cmd = exec.Command("bq", "load", "--schema=playdata_schema.json", ds_name+"."+mod_date, "gs://"+project_name+"-csv/"+mod_date+".csv")
+		failOnError(cmd.Run())
+		fmt.Println("bqへのロードを完了")
+	}
+	//ロードに使用したcsvファイルをgcsから削除する
+	cmd = exec.Command("gsutil", "rm", "gs://"+project_name+"-csv/"+mod_date+".csv")
+	failOnError(cmd.Run())
+	fmt.Println("gcs上のファイルの削除を完了")
 }
 
 func export_csv(mod_date string, playDataList []playData) {
