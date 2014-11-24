b672d47fb538eda1d0bbc5a855d7ef63976aab62
a-know <a.know.3373@gmail.com>
2014-11-24 22:22:18 +0900

    外部コマンド `gcloud` 関連の処理を切り出し

---
+++ external/gcloud.go
@@ -0,0 +1,20 @@
+package external
+
+import (
+	"os/exec"
+	"regexp"
+)
+
+func GetProjectName() (project_name string) {
+	cmd := exec.Command("gcloud", "config", "list")
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	util.FailOnError(cmd.Run())
+
+	//外部コマンドの実行結果からプロジェクト名を抽出する
+	reg, _ := regexp.Compile("\nproject = (.+)\n")
+	result := reg.Find([]byte(stdout.String()))
+	replace_reg, _ := regexp.Compile("project|\\s|\n|=")
+	project_name := replace_reg.ReplaceAllString(string(result), "")
+	return
+}
